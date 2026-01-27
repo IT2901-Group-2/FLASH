5b988f2db9d0a922501d86d87a0e683c85052f32
Igor Iwanicki <66352572+igiwa001@users.noreply.github.com>
2026-01-27 15:23:28 +0100

    fix: formatting (#42)

--- packages/ui/src/styles/colorType.ts
+++ packages/ui/src/styles/colorType.ts
@@ -1,8 +1,2 @@
 // Auto-generated color types
-export type ColorName =
-  | 'neutral'
-  | 'brand-purple'
-  | 'accent'
-  | 'success'
-  | 'warning'
-;
+export type ColorName = "neutral" | "brand-purple" | "accent" | "success" | "warning";
