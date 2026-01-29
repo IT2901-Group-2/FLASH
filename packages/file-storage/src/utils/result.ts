039e2bf3a87d24481c0e55a219123c1bf4625aa2
Igor Iwanicki <igor.iwanicki@protonmail.com>
2026-01-29 09:50:05 +0100

    extend awaited result type

--- packages/file-storage/src/utils/result.ts
+++ packages/file-storage/src/utils/result.ts
@@ -1,4 +1,4 @@
-import { Result } from "ts-results";
+import { Option, Result } from "ts-results";
 
 export type AwaitedResult<T, E> = Promise<Result<T, E>> & {
   map<T2>(func: (val: T) => T2): AwaitedResult<T2, E>;
@@ -6,6 +6,8 @@ export type AwaitedResult<T, E> = Promise<Result<T, E>> & {
   expect(msg: string): Promise<T>;
   unwrap(): Promise<T>;
   unwrapOr<T2>(val: T2): Promise<T | T2>;
+  andThen<T2, E2>(func: (val: T) => Result<T2, E2>): AwaitedResult<T2, E | E2>;
+  toOption(): Promise<Option<T>>;
 };
 
 export function awaited<T, E>(promise: Promise<Result<T, E>>): AwaitedResult<T, E> {
@@ -17,5 +19,8 @@ export function awaited<T, E>(promise: Promise<Result<T, E>>): AwaitedResult<T, 
     expect: (msg: string) => promise.then(r => r.expect(msg)),
     unwrap: () => promise.then(r => r.unwrap()),
     unwrapOr: <T2>(val: T2): Promise<T | T2> => promise.then(r => r.unwrapOr(val)),
+    andThen: <T2, E2>(func: (val: T) => Result<T2, E2>): AwaitedResult<T2, E | E2> =>
+      awaited<T2, E | E2>(promise.then(r => r.andThen(func))),
+    toOption: (): Promise<Option<T>> => promise.then(r => r.toOption()),
   });
 }
