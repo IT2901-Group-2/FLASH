1302ce820622a8c63c77939f0ebf563bb6965cac
Igor Iwanicki <igor.iwanicki@protonmail.com>
2026-02-02 20:25:12 +0100

    gcloud list directory

--- packages/file-storage/src/implementations/gcloudStorage.test.ts
+++ packages/file-storage/src/implementations/gcloudStorage.test.ts
@@ -1,13 +1,17 @@
 import { jest, afterEach, beforeEach, describe, expect, it } from "@jest/globals";
 import { GenericContainer, StartedTestContainer } from "testcontainers";
 import { Bucket, Storage } from "@google-cloud/storage";
+import { Result } from "typescript-result";
+import { GcloudStorage } from "./gcloudStorage";
 
-const image = new GenericContainer("igiwa001/google-storage-testbench");
 let container: StartedTestContainer;
 let bucket: Bucket;
 
 beforeEach(async () => {
-  container = await image.withExposedPorts(9000).withStartupTimeout(2000).start();
+  container = await new GenericContainer("igiwa001/google-storage-testbench")
+    .withExposedPorts(9000)
+    .withStartupTimeout(2000)
+    .start();
   bucket = await new Storage({
     projectId: "test-project",
     apiEndpoint: `http://${container.getHost()}:${container.getMappedPort(9000)}`,
@@ -21,8 +25,34 @@ afterEach(async () => {
   jest.restoreAllMocks();
 });
 
-describe("Temp", () => {
-  it("temp", async () => {
-    expect(bucket.name).toBe("test-bucket");
+describe("GcloudStorage list directory", () => {
+  it("Should return Err when listing non-existent directory", async () => {
+    Result.assertError(await new GcloudStorage(bucket).list("testdir"));
+  });
+
+  it("Should return empty list when listing empty directory", async () => {
+    const gcloudStorage = new GcloudStorage(bucket);
+
+    expect(await gcloudStorage.list("/").getOrThrow()).toStrictEqual([]);
+
+    await bucket.file("testdir/").save("");
+    expect(await gcloudStorage.list("testdir").getOrThrow()).toStrictEqual([]);
+  });
+
+  it("Should return correct file/directory names when listing directory", async () => {
+    await bucket.file("test/").save("");
+    await bucket.file("test/dir/").save("");
+    await bucket.file("test/dir/foo.txt").save("");
+    await bucket.file("test/dir/bar").save("");
+    await bucket.file("test/dir/baz/").save("");
+    await bucket.file("test/dir/baz/test.txt").save("");
+    await bucket.file("test/dir/dir2/").save("");
+
+    expect(
+      await new GcloudStorage(bucket)
+        .list("test/dir")
+        .map(f => new Set(f))
+        .getOrThrow()
+    ).toStrictEqual(new Set(["foo.txt", "bar", "baz/", "dir2/"]));
   });
 });
--- packages/file-storage/src/implementations/gcloudStorage.ts
+++ packages/file-storage/src/implementations/gcloudStorage.ts
@@ -2,7 +2,9 @@ import { Bucket } from "@google-cloud/storage";
 import { FileStorage } from "../interface";
 import { AsyncResult, Result } from "typescript-result";
 import { WithImplicitCoercion } from "node:buffer";
+import { dirPath, resolvePath } from "../utils";
 
+/* eslint-disable @typescript-eslint/no-unused-vars */
 export class GcloudStorage implements FileStorage {
   private bucket: Bucket;
 
@@ -11,9 +13,31 @@ export class GcloudStorage implements FileStorage {
   }
 
   list(path: string): AsyncResult<string[], Error> {
-    return Result.fromAsyncCatching(() => {
-      throw new Error("Not implemented");
-    });
+    const dirpath = dirPath(resolvePath(path)).replace(/^\//, "");
+
+    return Result.try(async () => {
+      if (dirpath === "") {
+        return;
+      }
+
+      const [exists] = await this.bucket.file(dirpath).exists();
+      if (!exists) {
+        throw new Error(`Directory ${dirpath} does not exist`);
+      }
+    })
+      .mapCatching(() =>
+        this.bucket.getFiles({
+          prefix: dirpath,
+          delimiter: "/",
+          includeTrailingDelimiter: true,
+        })
+      )
+      .map(res => res[0])
+      .map(files =>
+        files
+          .map(file => file.name.replace(new RegExp(`^${dirpath}`), ""))
+          .filter(file => file !== "")
+      );
   }
 
   read(path: string): AsyncResult<Buffer, Error> {
