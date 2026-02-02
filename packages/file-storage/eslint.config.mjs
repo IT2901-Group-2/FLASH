83541ac2beed97826346c29b4583112947a72edd
Igor Iwanicki <66352572+igiwa001@users.noreply.github.com>
2026-02-02 14:26:53 +0100

    Feat: File storage package (#45)
    * initial setup
    
    * jest setup
    
    * fsStorage implementation
    
    * filestorage delete function
    
    * awaited result
    
    * fsStorage test
    
    * linting fix
    
    * test file creation failing
    
    * switch to node test runner
    
    * restructure
    
    * extend awaited result type
    
    * Change error type
    
    * fsStorage list tests
    
    * FSStorage read tests
    
    * FSStorage mkdir tests
    
    * FSStorage write tests
    
    * FSStorage delete tests
    
    * switch to jest
    
    * enable test coverage reporting
    
    * switch to typescript-result
    
    * improve error messages
    
    * documentation
    
    * change blob to buffer
    
    * buffer conversion
    
    * improve tests
    
    * rename test folder
    
    * make fsStorage.dir private
    
    * small tweak
    
    * small improvements + documentation
    
    * remove custom error messages
    
    * documentation
    
    * switch to upath
    
    * fix linting command
    
    * make utils work on windows

---
+++ packages/file-storage/eslint.config.mjs
@@ -0,0 +1,23 @@
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import { defineConfig, globalIgnores } from "eslint/config";
+
+const eslintConfig = defineConfig([
+  globalIgnores(["node_modules/**"]),
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          varsIgnorePattern: "_",
+          argsIgnorePattern: "_",
+          caughtErrorsIgnorePattern: "_",
+        },
+      ],
+    },
+  },
+]);
+
+export default eslintConfig;
