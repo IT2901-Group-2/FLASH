aca23fb63d3c4783150374af3209b379a7b752ec
Gnister <82974914+GnisterDev@users.noreply.github.com>
2026-02-02 16:11:45 +0100

    Fixed import `@storybook/vite` to `@storybook/vite-react` (#68)

--- packages/ui/src/components/ImageCard/ImageCard.stories.tsx
+++ packages/ui/src/components/ImageCard/ImageCard.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from "@storybook/react";
+import type { Meta, StoryObj } from "@storybook/react-vite";
 import { ImageCard } from "./ImageCard";
 import { expect, fn, userEvent, within } from "storybook/test";
 import { Heart, Star, Camera } from "lucide-react";
