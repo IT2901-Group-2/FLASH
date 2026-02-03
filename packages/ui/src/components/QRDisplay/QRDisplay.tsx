c676a4108846677d464fd5903631369de585c7c4
Sindre Augustin Schärer <sscharer@online.no>
2026-02-03 23:17:29 +0100

    Fixed colors when in dark mode

--- packages/ui/src/components/QRDisplay/QRDisplay.tsx
+++ packages/ui/src/components/QRDisplay/QRDisplay.tsx
@@ -15,7 +15,13 @@ export interface QRDisplayProps extends React.HTMLAttributes<HTMLDivElement> {
 const QRDisplay = ({ value, code, ...rest }: QRDisplayProps) => {
   return (
     <div className={styles.container} {...rest}>
-      <QRCode value={value} level="H" className={styles.qrCode} />
+      <QRCode
+        value={value}
+        level="H"
+        className={styles.qrCode}
+        bgColor={`var(--color-neutral-000)`}
+        fgColor={`var(--color-neutral-1000)`}
+      />
       <div className={styles.text}>
         {code && <span className={styles.code}>{code}</span>}
         <span className={styles.helperText}>Scan to upload photos</span>
