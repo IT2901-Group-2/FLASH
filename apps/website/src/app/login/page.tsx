5d2397bb0e2a9a7b9c26879ddd5baebf2505a7e5
Philip Torvund Bennett <philitb@stud.ntnu.no>
2026-02-04 10:12:22 +0100

    Small fix for using storybook components

---
+++ apps/website/src/app/login/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+import { Title } from "ui";
+
+const page = () => {
+  return (
+    <div>
+      <Title>Login page</Title>
+    </div>
+  );
+};
+
+export default page;
--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -18,6 +18,7 @@ export default defineConfig({
     react(),
     dts({
       include: ["src"],
+      insertTypesEntry: true,
     }),
     libCss(),
   ],
