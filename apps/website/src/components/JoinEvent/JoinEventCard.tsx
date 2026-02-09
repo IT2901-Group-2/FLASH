2de006c2556c5270d136f62ad333f693fddf40f0
Karl Frostrud <karlfrostrud@Karl-sin-MacBook-Air.local>
2026-02-09 20:39:23 +0100

    Create page and the join event card

---
+++ apps/website/src/components/JoinEvent/JoinEventCard.tsx
@@ -0,0 +1,15 @@
+"use client";
+import { Card, Input, Button, Title } from "ui";
+
+const JoinEventCard = () => {
+
+    return (
+        <Card data-color="background-secondary">
+            <Title as="h2" description="Enter the event code or scan the QR code to join">Join an event</Title>
+        </Card>
+    )
+}
+
+export default JoinEventCard;
+
+
