c23ab0ce6e899117d2ceece6a6ceffd34760bcdc
Gnister <sscharer@online.no>
2026-02-11 20:20:02 +0100

    Fix to logo className property

--- apps/website/src/components/Logo/Logo.tsx
+++ apps/website/src/components/Logo/Logo.tsx
@@ -1,10 +1,7 @@
 import { Camera } from "lucide-react";
 import styles from "./Logo.module.css";
 
-export default function Logo({
-  className,
-  ...rest
-}: React.HTMLAttributes<HTMLDivElement>) {
+export default function Logo({ ...rest }: React.HTMLAttributes<HTMLDivElement>) {
   return (
     <div className={styles.cameraWrapper} {...rest}>
       <Camera className={styles.camera} />
--- apps/website/src/components/Sidebar/SidebarHeader.tsx
+++ apps/website/src/components/Sidebar/SidebarHeader.tsx
@@ -4,7 +4,7 @@ import styles from "./SidebarHeader.module.css";
 export const SidebarHeader = () => {
   return (
     <div className={styles.header}>
-      <Logo className={styles.logo} />
+      <Logo />
       <div className={styles.headerText}>
         <h3 className={styles.title}>PhotoEvent</h3>
         <span>Admin Panel</span>
