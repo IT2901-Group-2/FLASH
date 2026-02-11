8f8a53f035e3d4e878a9f0419e4c0adfe42a24f8
Gnister <sscharer@online.no>
2026-02-11 19:43:26 +0100

    Added `...rest` to logo

--- apps/website/src/components/Logo/Logo.tsx
+++ apps/website/src/components/Logo/Logo.tsx
@@ -1,9 +1,9 @@
 import { Camera } from "lucide-react";
 import styles from "./Logo.module.css";
 
-export default function Logo() {
+export default function Logo({ ...rest }) {
   return (
-    <div className={styles.cameraWrapper}>
+    <div className={styles.cameraWrapper} {...rest}>
       <Camera className={styles.camera} />
     </div>
   );
