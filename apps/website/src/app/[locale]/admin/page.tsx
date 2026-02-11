e630a0099cf921afe8302758f16e847eece54c88
Karl Frostrud <karlfrostrud@dhcp-10-220-229-245.wlan.ntnu.no>
2026-02-11 14:59:06 +0100

    Move adminlogin into the locale folder

---
+++ apps/website/src/app/[locale]/admin/page.tsx
@@ -0,0 +1,24 @@
+"use client";
+import SignInCard from "@/components/SignInCard/SignInCard";
+import { Title } from "ui";
+import styles from "./AdminLogin.module.css";
+import CameraIcon from "@/components/Logo/Logo";
+
+export default function Page() {
+  return (
+    <div className={styles.pageWrapper}>
+      <CameraIcon />
+      <Title
+        align="center"
+        size="large"
+        as="h1"
+        data-color="brand-purple"
+        description="Manage your photo events with ease"
+      >
+        PhotoEvent Admin
+      </Title>
+      <SignInCard />
+      <p>Self-hosted Photo Event Management System</p>
+    </div>
+  );
+}
