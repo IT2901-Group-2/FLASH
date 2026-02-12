14806744cca1b5734cb28d8ece4638f5466ac892
Gnister <sscharer@online.no>
2026-02-12 20:56:41 +0100

    Added string util

--- apps/website/src/utils/string-utils.ts
+++ apps/website/src/utils/string-utils.ts
@@ -7,3 +7,22 @@
 export const capitalize = (string: string) => {
   return string.charAt(0).toUpperCase() + string.slice(1);
 };
+
+/**
+ * Generates a random string of a given length
+ * Uses only alphabetical charachters (a-zA-Z).
+ *
+ * @param length The length the outputed string will be
+ * @returns A random string of the specified length
+ */
+export function generateRandomString(length: number): string {
+  const characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let result = "";
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    result += characters[randomIndex];
+  }
+
+  return result;
+}
