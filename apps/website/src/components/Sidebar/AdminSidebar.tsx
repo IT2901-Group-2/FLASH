68ff3dbc71edfbf9c136892fdd4960627a32dba5
Gnister <sscharer@online.no>
2026-02-12 11:14:35 +0100

    Added translations

--- apps/website/src/components/Sidebar/AdminSidebar.tsx
+++ apps/website/src/components/Sidebar/AdminSidebar.tsx
@@ -2,11 +2,13 @@ import { Calendar, HardDrive, House, Settings } from "lucide-react";
 import { Sidebar } from "ui";
 import SidebarFooter from "./SidebarFooter";
 import { SidebarHeader } from "./SidebarHeader";
+import { useTranslations } from "next-intl";
 
 /**
  * Admin sidebar used in the /admin/dashboard pages
  */
 export const AdminSidebar = () => {
+  const t = useTranslations("admin.dashboard.sidebar");
   return (
     <Sidebar>
       <Sidebar.Trigger style={{ top: "3.5rem" }} />{" "}
@@ -15,12 +17,12 @@ export const AdminSidebar = () => {
         <SidebarHeader />
       </Sidebar.Header>
       <Sidebar.Group title="MAIN">
-        <Sidebar.Item icon={<House />}>Desktop</Sidebar.Item>
-        <Sidebar.Item icon={<Calendar />}>Event</Sidebar.Item>
+        <Sidebar.Item icon={<House />}>{t("desktop")}</Sidebar.Item>
+        <Sidebar.Item icon={<Calendar />}>{t("event")}</Sidebar.Item>
       </Sidebar.Group>
       <Sidebar.Group title="Config" position="bottom">
-        <Sidebar.Item icon={<Settings />}>Settings</Sidebar.Item>
-        <Sidebar.Item icon={<HardDrive />}>Storage</Sidebar.Item>
+        <Sidebar.Item icon={<Settings />}>{t("settings")}</Sidebar.Item>
+        <Sidebar.Item icon={<HardDrive />}>{t("storage")}</Sidebar.Item>
       </Sidebar.Group>
       <Sidebar.Footer>
         <SidebarFooter />
