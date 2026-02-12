5ba78b79cdf59c364455c04296814a014089244d
Gnister <82974914+GnisterDev@users.noreply.github.com>
2026-02-12 13:39:24 +0100

    Feat/website/sidebar mode toggle (#99)
    * Dark/Light mode switch
    
    * Theme switching
    
    * Added test to `string-utils.ts`

--- apps/website/src/components/Sidebar/AdminSidebar.tsx
+++ apps/website/src/components/Sidebar/AdminSidebar.tsx
@@ -1,15 +1,18 @@
-import { Calendar, HardDrive, House, Settings } from "lucide-react";
+import { Calendar, HardDrive, House, Moon, Settings, Sun } from "lucide-react";
 import { Sidebar } from "ui";
 import SidebarFooter from "./SidebarFooter";
 import { SidebarHeader } from "./SidebarHeader";
 import { useTranslations } from "next-intl";
 import { HTMLAttributes } from "react";
+import { useTheme } from "@/hooks/useTheme";
+import { capitalize } from "@/utils/string-utils";
 
 /**
  * Admin sidebar used in the /admin/dashboard pages
  */
 export const AdminSidebar = ({ className, ...rest }: HTMLAttributes<HTMLDivElement>) => {
   const t = useTranslations("admin.dashboard.sidebar");
+  const { theme, toggleTheme } = useTheme();
 
   return (
     <Sidebar className={className} {...rest}>
@@ -25,6 +28,9 @@ export const AdminSidebar = ({ className, ...rest }: HTMLAttributes<HTMLDivEleme
       <Sidebar.Group title="Config" position="bottom">
         <Sidebar.Item icon={<Settings />}>{t("settings")}</Sidebar.Item>
         <Sidebar.Item icon={<HardDrive />}>{t("storage")}</Sidebar.Item>
+        <Sidebar.Item icon={theme === "dark" ? <Sun /> : <Moon />} onClick={toggleTheme}>
+          {capitalize(theme)} mode
+        </Sidebar.Item>
       </Sidebar.Group>
       <Sidebar.Footer>
         <SidebarFooter />
