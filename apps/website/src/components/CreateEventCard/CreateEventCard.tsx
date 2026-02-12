59be7bcf81668ba6b611be4b10fbd71dbdab7293
Gnister <sscharer@online.no>
2026-02-12 13:59:01 +0100

    Added steps to CreateEventCard

--- apps/website/src/components/CreateEventCard/CreateEventCard.tsx
+++ apps/website/src/components/CreateEventCard/CreateEventCard.tsx
@@ -1,5 +1,5 @@
 import { RefAttributes, useState } from "react";
-import { Button, Card, Input, ProgressDots, Title } from "ui";
+import { Button, Card, Controls, Input, ProgressDots, Title } from "ui";
 import styles from "./CreateEventCard.module.css";
 import { Calendar } from "lucide-react";
 
@@ -7,29 +7,54 @@ interface CreateEventCardProps extends RefAttributes<HTMLDialogElement> {
   onClose?: () => void;
 }
 
+const Step1 = () => {
+  return (
+    <>
+      <Title description="Set up a new photo event for your guests. A QR code will be generated automatically.">
+        Create Event Name
+      </Title>
+      <Input label="Event Name" aria-label="eventName" />
+      <Input label="Event Description" aria-label="eventDescription" />
+      <Input label="Event Date" aria-label="eventDate" type="date" icon={<Calendar />} />
+    </>
+  );
+};
+
+const Step2 = () => {
+  return (
+    <>
+      <Controls
+        options={[
+          {
+            label: "Enable",
+            value: "enable",
+          },
+          {
+            label: "Disable",
+            value: "disable",
+          },
+        ]}
+      />
+    </>
+  );
+};
+
 export const CreateEventCard = ({ ref, onClose, ...rest }: CreateEventCardProps) => {
-  const [progress, setProgress] = useState<number>(1);
+  const [progress, setProgress] = useState<number>(2);
+
+  const steps = [Step1, Step2] as const;
+  const CurrentStep = steps[progress - 1];
 
   return (
     <dialog ref={ref} className={styles.container} {...rest} autoFocus>
       <Card className={styles.card}>
         <ProgressDots maxValue={3} value={progress} data-color="brand-purple" />
-        <Title description="Set up a new photo event for your guests. A QR code will be generated automatically.">
-          Create Event Name
-        </Title>
-        <Input label="Event Name" aria-label="eventName" />
-        <Input label="Event Description" aria-label="eventDescription" />
-        <Input
-          label="Event Date"
-          aria-label="eventDate"
-          type="date"
-          icon={<Calendar />}
-        />
+        <CurrentStep />
         <div className={styles.buttonGroup}>
           <Button variant="tertiary" onClick={onClose}>
             Cancel
           </Button>
-          <Button variant="secondary">Next</Button>
+          {progress < steps.length && <Button variant="secondary">Next</Button>}
         </div>
       </Card>
     </dialog>
