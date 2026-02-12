ca22ed411060c9b6bdc272289a8002ffc47fd84b
Gnister <sscharer@online.no>
2026-02-12 12:26:36 +0100

    Page for events

--- apps/website/src/app/[locale]/admin/dashboard/event/page.tsx
+++ apps/website/src/app/[locale]/admin/dashboard/event/page.tsx
@@ -1,10 +1,17 @@
 "use client";
-import { Title } from "ui";
+import { Plus } from "lucide-react";
+import { Button, Title } from "ui";
+import styles from "./page.module.css";
 
 export const Page = () => {
   return (
     <>
-      <Title description="Manage your events.">Event</Title>
+      <div className={styles.header}>
+        <Title description="Manage your events.">Event</Title>
+        <Button icon={<Plus />} data-color="brand-purple">
+          Create New Event
+        </Button>
+      </div>
     </>
   );
 };
