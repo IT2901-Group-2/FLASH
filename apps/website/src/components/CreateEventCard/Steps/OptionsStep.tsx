25d58434a914f589da542ba498bdec125730dc1f
Gnister <82974914+GnisterDev@users.noreply.github.com>
2026-02-13 22:06:28 +0100

    Event page for creating and showing events (#100)
    * Start of event-page
    
    * Page for events
    
    * First page of create Event
    
    * Added Controls to ui export
    
    * Added steps to CreateEventCard
    
    * Fixed theme switching
    
    * Added helper functions
    
    * Override defalt button type of "submit" in forms
    
    * Remove `max-width` in controls group
    
    * Fixed card moving when active
    
    * fix sidebar scale
    
    * Finished step 1 with form validation
    
    * Remove width from DropdownControls
    
    * Fixed import from ui
    
    * Finished Page2 of CreateEventCard
    
    * Fixed `ProgressDot` z-fighting
    
    * Extended time for line
    
    * Changed timing of ProgressDots
    
    * Adde QRCodeSisplay to output
    
    * Added string util
    
    * Event from done
    
    * CreateEventCard buttons
    
    * Updated `string-utils.test.tsx`
    
    * Translations
    
    * removed font size in body
    
    * Fix css with dialog overflowing
    
    * Changed 9999 to Infinity in maxPhotos
    
    * removed export from page. Now only exported as default.
    
    * Changed other export Page
    
    * Removed `console.log`
    
    * Removed commented out sections
    
    * Fixed `generateRandomString(length)`

---
+++ apps/website/src/components/CreateEventCard/Steps/OptionsStep.tsx
@@ -0,0 +1,68 @@
+import { useState } from "react";
+import { StepProps } from "../CreateEventCard";
+import { Title, DropdownControls, Input, Switch } from "ui";
+import styles from "./Steps.module.css";
+import { useTranslations } from "next-intl";
+
+export const OptionsStep = ({ formData, updateFormData }: StepProps) => {
+  const t = useTranslations("admin.dashboard.event.create.options");
+
+  const [limitMode, setLimitMode] = useState<"limited" | "unlimited">("limited");
+  const [autoGenerateCode, setAutoGenerateCode] = useState<boolean>(true);
+
+  return (
+    <>
+      <Title description={t("description")}>{t("title")}</Title>
+      <DropdownControls
+        onChange={setLimitMode}
+        options={[
+          {
+            content: (
+              <div className={styles.maxImageContainer}>
+                <span>{t("input.uploads.title")}</span>
+                <Input
+                  aria-label="maxImages"
+                  type="number"
+                  min={1}
+                  value={formData.photosPerGuest}
+                  onChange={e =>
+                    updateFormData(
+                      "photosPerGuest",
+                      limitMode === "limited" ? e.target.value : Infinity
+                    )
+                  }
+                />
+              </div>
+            ),
+            label: t("input.uploads.limited"),
+            value: "limited",
+          },
+          {
+            label: t("input.uploads.unlimited"),
+            value: "unlimited",
+          },
+        ]}
+      />
+      <Switch position="right">
+        <b>{t("input.autoApprove")}</b>
+      </Switch>
+      <Switch
+        position="right"
+        checked={autoGenerateCode}
+        onChange={e => setAutoGenerateCode(e.target.checked)}
+      >
+        <b>{t("input.autoGenerate")}</b>
+      </Switch>
+      {!autoGenerateCode && (
+        <Input
+          visualSize="small"
+          label="Custom Entry Code"
+          aria-label="manualCode"
+          onChange={e => updateFormData("code", e.target.value)}
+        />
+      )}
+    </>
+  );
+};
+
+export default OptionsStep;
