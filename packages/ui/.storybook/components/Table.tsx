694996ce36a672e0083144b6a8ea96d1f6173ab0
Gnister <sscharer@online.no>
2026-02-15 01:20:28 +0100

    Fixed table prop for storybook

--- packages/ui/.storybook/components/Table.tsx
+++ packages/ui/.storybook/components/Table.tsx
@@ -1,24 +1,26 @@
 import styles from "./Table.module.css";
 
 export type TableData = {
-  header: string[];
-  data: Array<{ [key: string]: string }>;
+  data: {
+    header: string[];
+    valuePairs: Array<{ [key: string]: string }>;
+  };
 };
 
-export const Table = ({ header, data }: TableData) => {
-  const dataKeys = data.length > 0 ? Object.keys(data[0]) : [];
+export const Table = ({ data }: TableData) => {
+  const dataKeys = data.valuePairs.length > 0 ? Object.keys(data.valuePairs[0]) : [];
 
   return (
     <table className={styles.table}>
       <thead>
         <tr>
-          {header.map((headerItem, index) => (
+          {data.header.map((headerItem, index) => (
             <th key={index}>{headerItem}</th>
           ))}
         </tr>
       </thead>
       <tbody>
-        {data.map((row, i) => (
+        {data.valuePairs.map((row, i) => (
           <tr key={i}>
             {dataKeys.map((key, index) => (
               <td key={index}>{row[key]}</td>
