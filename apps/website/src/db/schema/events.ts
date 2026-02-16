1a8e6723a5c7f65f8044e11df732ff3dc2fd66b8
Igor Iwanicki <igor.iwanicki@protonmail.com>
2026-02-16 15:45:53 +0100

    update event table

--- apps/website/src/db/schema/events.ts
+++ apps/website/src/db/schema/events.ts
@@ -1,17 +1,33 @@
-import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
-import { z } from "zod";
+import { integer, sqliteTable, text, check } from "drizzle-orm/sqlite-core";
 import ShortUniqueId from "short-unique-id";
+import { lte } from "drizzle-orm";
+import { z } from "zod";
 
 const uid = new ShortUniqueId();
 const code = new ShortUniqueId({ length: 6, dictionary: "alphanum_upper" });
 
-export const eventTable = sqliteTable("events", {
-  id: text().primaryKey().$defaultFn(uid.rnd),
-  name: text().notNull(),
-  uploadLimit: integer(),
-  guestCode: text().unique().notNull().$defaultFn(code.rnd),
-  adminCode: text().unique().notNull().$defaultFn(code.rnd),
-});
+export const eventTable = sqliteTable(
+  "events",
+  {
+    id: text().primaryKey().$defaultFn(uid.rnd),
+    name: text().notNull(),
+    description: text().notNull().default(""),
+    startDate: integer({ mode: "timestamp" }).notNull(),
+    endDate: integer({ mode: "timestamp" }).notNull(),
+    uploadLimit: integer(),
+    guestCode: text().unique().notNull().$defaultFn(code.rnd),
+    moderatorCode: text().unique().notNull().$defaultFn(code.rnd),
+    createdAt: integer({ mode: "timestamp" })
+      .notNull()
+      .$defaultFn(() => new Date()),
+    updatedAt: integer({ mode: "timestamp" })
+      .notNull()
+      .$defaultFn(() => new Date())
+      .$onUpdate(() => new Date()),
+    isArchived: integer({ mode: "boolean" }).default(false),
+  },
+  t => [check("dateConstraint", lte(t.startDate, t.endDate))]
+);
 
 export const getEventSchema = z.object({
   id: z.string().array().min(1).optional(),
@@ -23,7 +39,7 @@ export const getEventSchema = z.object({
     .tuple([z.string()])
     .transform(([str]) => str)
     .optional(),
-  adminCode: z
+  moderatorCode: z
     .tuple([z.string()])
     .transform(([str]) => str)
     .optional(),
@@ -31,11 +47,16 @@ export const getEventSchema = z.object({
 
 export const createEventSchema = z.object({
   name: z.string(),
+  description: z.string().optional(),
+  startDate: z.coerce.date(),
+  endDate: z.coerce.date(),
   uploadLimit: z.number().positive().optional(),
 });
 
 export const updateEventSchema = z.object({
   name: z.string().optional(),
+  startDate: z.coerce.date().optional(),
+  endDate: z.coerce.date().optional(),
   uploadLimit: z.number().optional(),
 });
 
--- apps/website/src/services/eventService.ts
+++ apps/website/src/services/eventService.ts
@@ -2,7 +2,7 @@ import { Database, DatabaseService, dbService } from "./databaseService";
 import { CreateEvent, Event, eventTable, GetEvent, UpdateEvent } from "@/db";
 import { AsyncResult, Result } from "typescript-result";
 import { getFirstRow } from "@/lib/utils/sql";
-import { and, eq, like, inArray } from "drizzle-orm";
+import { sql, and, eq, like, inArray } from "drizzle-orm";
 
 export class EventService {
   private readonly dbService: DatabaseService;
@@ -13,7 +13,12 @@ export class EventService {
     this.db = dbService.db;
   }
 
-  getEvents({ id, name, guestCode, adminCode }: GetEvent): AsyncResult<Event[], Error> {
+  getEvents({
+    id,
+    name,
+    guestCode,
+    moderatorCode,
+  }: GetEvent): AsyncResult<Event[], Error> {
     return Result.try(() =>
       this.db
         .select()
@@ -21,9 +26,11 @@ export class EventService {
         .where(
           and(
             id !== undefined ? inArray(eventTable.id, id) : undefined,
-            name !== undefined ? like(eventTable.name, `%${name}%`) : undefined,
+            name !== undefined ? like(eventTable.name, sql`%${name}%`) : undefined,
             guestCode !== undefined ? eq(eventTable.guestCode, guestCode) : undefined,
-            adminCode !== undefined ? eq(eventTable.adminCode, adminCode) : undefined
+            moderatorCode !== undefined
+              ? eq(eventTable.moderatorCode, moderatorCode)
+              : undefined
           )
         )
     );
