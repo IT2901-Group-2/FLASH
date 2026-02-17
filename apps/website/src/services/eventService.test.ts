0342d0d43f04432dd866f90478fc769ecbb24a36
Igor Iwanicki <igor.iwanicki@protonmail.com>
2026-02-17 19:26:11 +0100

    eventService test isArchived

--- apps/website/src/services/eventService.test.ts
+++ apps/website/src/services/eventService.test.ts
@@ -11,12 +11,22 @@ const NOW = new Date();
 const mockEvents: (typeof eventTable.$inferInsert)[] = [
   {
     id: "birthday-1",
-    name: "Birthday",
+    name: "Birthday 1",
     startDate: setHours(NOW, 11),
     endDate: setHours(NOW, 14),
     uploadLimit: 5,
     guestCode: "birthday-1-guest",
     moderatorCode: "birthday-1-moderator",
+  },
+  {
+    id: "birthday-2",
+    name: "Birthday 2",
+    startDate: setHours(NOW, 11),
+    endDate: setHours(NOW, 14),
+    uploadLimit: 5,
+    guestCode: "birthday-2-guest",
+    moderatorCode: "birthday-2-moderator",
+    isArchived: true,
   },
   {
     id: "wedding-1",
@@ -66,28 +76,24 @@ beforeEach(async () => {
 });
 
 describe("EventService getEvents", () => {
-  it("Should get all mock data", async () => {
-    expect(await eventService.getEvents().getOrThrow()).toHaveLength(mockEvents.length);
-  });
-
   it("Should correctly filter by id", async () => {
     expect(
       await eventService
-        .getEvents({ id: ["unknown-id"] })
+        .getEvents({ id: ["unknown-id"], archived: false })
         .map(rows => new Set(rows.map(row => row.id)))
         .getOrThrow()
     ).toStrictEqual(new Set([]));
 
     expect(
       await eventService
-        .getEvents({ id: ["wedding-1"] })
+        .getEvents({ id: ["wedding-1"], archived: false })
         .map(rows => new Set(rows.map(row => row.id)))
         .getOrThrow()
     ).toStrictEqual(new Set(["wedding-1"]));
 
     expect(
       await eventService
-        .getEvents({ id: ["uppercase-1", "birthday-1"] })
+        .getEvents({ id: ["uppercase-1", "birthday-1"], archived: false })
         .map(rows => new Set(rows.map(row => row.id)))
         .getOrThrow()
     ).toStrictEqual(new Set(["uppercase-1", "birthday-1"]));
@@ -96,35 +102,35 @@ describe("EventService getEvents", () => {
   it("Should correctly filter by name", async () => {
     expect(
       await eventService
-        .getEvents({ name: "Birthday" })
+        .getEvents({ name: "Birthday", archived: false })
         .map(rows => new Set(rows.map(row => row.id)))
         .getOrThrow()
     ).toStrictEqual(new Set(["birthday-1"]));
 
     expect(
       await eventService
-        .getEvents({ name: "eddi" })
+        .getEvents({ name: "eddi", archived: false })
         .map(rows => new Set(rows.map(row => row.id)))
         .getOrThrow()
     ).toStrictEqual(new Set(["wedding-1", "wedding-2"]));
 
     expect(
       await eventService
-        .getEvents({ name: "gibberish" })
+        .getEvents({ name: "gibberish", archived: false })
         .map(rows => new Set(rows.map(row => row.id)))
         .getOrThrow()
     ).toStrictEqual(new Set([]));
 
     expect(
       await eventService
-        .getEvents({ name: "LOwErCaSE" })
+        .getEvents({ name: "LOwErCaSE", archived: false })
         .map(rows => new Set(rows.map(row => row.id)))
         .getOrThrow()
     ).toStrictEqual(new Set(["lowercase-1"]));
 
     expect(
       await eventService
-        .getEvents({ name: "uppercase" })
+        .getEvents({ name: "uppercase", archived: false })
         .map(rows => new Set(rows.map(row => row.id)))
         .getOrThrow()
     ).toStrictEqual(new Set(["uppercase-1"]));
@@ -133,14 +139,14 @@ describe("EventService getEvents", () => {
   it("Should correctly filter by guest code", async () => {
     expect(
       await eventService
-        .getEvents({ guestCode: "birthday-1-moderator" })
+        .getEvents({ guestCode: "birthday-1-moderator", archived: false })
         .map(rows => new Set(rows.map(row => row.id)))
         .getOrThrow()
     ).toStrictEqual(new Set([]));
 
     expect(
       await eventService
-        .getEvents({ guestCode: "birthday-1-guest" })
+        .getEvents({ guestCode: "birthday-1-guest", archived: false })
         .map(rows => new Set(rows.map(row => row.id)))
         .getOrThrow()
     ).toStrictEqual(new Set(["birthday-1"]));
@@ -149,16 +155,50 @@ describe("EventService getEvents", () => {
   it("Should correctly filter by moderator code", async () => {
     expect(
       await eventService
-        .getEvents({ moderatorCode: "wedding-2-guest" })
+        .getEvents({ moderatorCode: "wedding-2-guest", archived: false })
         .map(rows => new Set(rows.map(row => row.id)))
         .getOrThrow()
     ).toStrictEqual(new Set([]));
 
     expect(
       await eventService
-        .getEvents({ moderatorCode: "wedding-2-moderator" })
+        .getEvents({ moderatorCode: "wedding-2-moderator", archived: false })
         .map(rows => new Set(rows.map(row => row.id)))
         .getOrThrow()
     ).toStrictEqual(new Set(["wedding-2"]));
+  });
+
+  it("Should correctly filter by isArchived", async () => {
+    expect(
+      await eventService
+        .getEvents()
+        .map(rows => new Set(rows.map(row => row.id)))
+        .getOrThrow()
+    ).toStrictEqual(
+      new Set([
+        "birthday-1",
+        "birthday-2",
+        "wedding-1",
+        "wedding-2",
+        "lowercase-1",
+        "uppercase-1",
+      ])
+    );
+
+    expect(
+      await eventService
+        .getEvents({ archived: true })
+        .map(rows => new Set(rows.map(row => row.id)))
+        .getOrThrow()
+    ).toStrictEqual(new Set(["birthday-2"]));
+
+    expect(
+      await eventService
+        .getEvents({ archived: false })
+        .map(rows => new Set(rows.map(row => row.id)))
+        .getOrThrow()
+    ).toStrictEqual(
+      new Set(["birthday-1", "wedding-1", "wedding-2", "lowercase-1", "uppercase-1"])
+    );
   });
 });
