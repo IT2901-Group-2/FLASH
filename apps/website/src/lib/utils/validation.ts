7e6986694509aeaf8055f0dce78f9c74377be046
Igor Iwanicki <igor.iwanicki@protonmail.com>
2026-02-17 20:49:39 +0100

    validation comments

--- apps/website/src/lib/utils/validation.ts
+++ apps/website/src/lib/utils/validation.ts
@@ -2,6 +2,13 @@ import { NextRequest } from "next/server";
 import { AsyncResult, Result } from "typescript-result";
 import z from "zod";
 
+/**
+ * Utility function to validate a `NextRequest` body against a `zod` schema.
+ *
+ * @param request The request whose body to validate.
+ * @param schema The `zod` schema to validate against.
+ * @returns A result with the parsed result body or an error.
+ */
 export function parseRequestBody<T>(
   request: NextRequest,
   schema: z.ZodType<T>
@@ -9,6 +16,13 @@ export function parseRequestBody<T>(
   return Result.try(async () => z.parseAsync(schema, await request.json()));
 }
 
+/**
+ * Utility function to validate `URLSearchParams` against a `zod` schema.
+ *
+ * @param searchParams The `URLSearchParams` object to validate.
+ * @param schema The `zod` schema to validate against.
+ * @returns A result with the search params or an error.
+ */
 export function parseSearchParams<T>(
   searchParams: URLSearchParams,
   schema: z.ZodType<T>
