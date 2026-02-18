831c1a709a8879e63050e39fb94645b71d26dc88
Igor Iwanicki <igor.iwanicki@protonmail.com>
2026-02-18 14:39:25 +0100

    imageService comments

--- apps/website/src/services/imageService.ts
+++ apps/website/src/services/imageService.ts
@@ -19,19 +19,42 @@ export class ImageService {
     this.storage = storage;
   }
 
+  /**
+   * Attempts to convert the blob into a wepb image.
+   *
+   * @param image The blob to convert.
+   * @returns A result contianing a webp image as a buffer or an error.
+   */
   private convertImage(image: Blob): AsyncResult<Buffer<ArrayBufferLike>, Error> {
     return Result.try(() => image.bytes()).mapCatching(buf =>
       sharp(buf).webp().toBuffer()
     );
   }
 
+  /**
+   * Returns a list of all images associated with the specified event.
+   *
+   * @param eventId The id of the event.
+   * @returns A result containing a list of `Image` objects or an error.
+   */
   getImages(eventId: string): AsyncResult<Image[], Error> {
     return Result.try(() =>
       this.dbService.db.select().from(imageTable).where(eq(imageTable.eventId, eventId))
     );
   }
 
-  downloadImage(eventId: string, imageId: string): AsyncResult<Buffer, Error> {
+  /**
+   * Downloads the image with the specified id.
+   * Will fail if the image does not exist or does not belong to the specified event.
+   *
+   * @param eventId The id of the event the image belongs to.
+   * @param imageId The id of the image to download.
+   * @returns A result containing the downloaded image as a `Buffer` or an error
+   */
+  downloadImage(
+    eventId: string,
+    imageId: string
+  ): AsyncResult<Buffer<ArrayBufferLike>, Error> {
     return Result.try(() =>
       this.dbService.db
         .select()
@@ -47,6 +70,13 @@ export class ImageService {
       .map(() => this.storage.read(`${imageId}.webp`));
   }
 
+  /**
+   * Uploads the given image blob to the specified event.
+   *
+   * @param eventId The id of the event to upload the image into.
+   * @param image The image to upload.
+   * @returns A result containing the newly inserted `Image` object or an error.
+   */
   uploadImage(eventId: string, image: Blob): AsyncResult<Image, Error> {
     const id = uid.rnd();
 
