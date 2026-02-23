20c6e3c23b03b2ebf67e249013edc62979971f02
Igor Iwanicki <igor.iwanicki@protonmail.com>
2026-02-23 17:18:05 +0100

    imageService test add mock data

--- apps/website/src/services/imageService.test.ts
+++ apps/website/src/services/imageService.test.ts
@@ -28,12 +28,38 @@ const mockEvents: (typeof eventTable.$inferInsert)[] = [
   },
 ];
 
-type MockImage = typeof imageTable.$inferInsert & { imageData: string };
+const mockImageData: Buffer[] = [
+  "UklGRpgAAABXRUJQVlA4IIwAAABwAgCdASoPAA8AAUAmJbACdAYwd2dm7KLX2flgAP71vYHO4Y3N7L8m20BqoD/9s8W6f7MwJ47iUuo6mo2cLVxnuo/O20m4Gw84D3fjBDBqrkZpcQ0r9QxvnZ30EnpHY7HYFVsqel1JCusf9WgFrW2TL+/cRGtH+A/46tFARCaB/kmzFBzXhGpNF1LAAA==",
+  "UklGRpoAAABXRUJQVlA4II4AAABwAgCdASoPAA8AAUAmJYwCdAYuvmpigIIQjnUAAP7X3TXC7NYF9lM269VWbhJFInjaMjPlYFaMX0iafgeTq4jD+TCg0d/K62oKUEZKQYwP5X6Y8xR3FpR+jeairJWRQPE2KoQIMB4bjYgHp/OiXLO1fhXLg3tfoftmGJJBqJPDWYDitzMRK6DIFqVf9IgA",
+  "UklGRrQAAABXRUJQVlA4IKgAAACwAgCdASoPAA8AAUAmJbACdLoANsBjw/ZnN7hJKAAA/t2flkJn/P9VuS7HH8IUD0nMr8GM6oGsKTBi6VNnDJHUDOxvj908s3cUABG+UeNOhYgr3gmSjSbOcGuYWxyFEQc9h7X+xyms4V94wZF3hAW5GU7Y3O5L+HBgyd69Of//hpe5He94h0+1stP2wnwF5xHZO/8SwLjeYc3+M9KbYYFOzrSdDHNAAAA=",
+  "UklGRrIAAABXRUJQVlA4IKYAAACQAgCdASoPAA8AAUAmJYgCdDiMxykHTy50HtO8AAD++8G1B580sjDGPpDh93uU+bgfxCEm/3+2tc7RN22rSG4FqMYjjOvIZWgD/ZNezv/KyS2YaqxgJpl7uLr86Vw++V5DT9NfCapWpDoYoXSzivpGlEzsbSZS0xeHmlyxvl+v7f5jrsiEb7/gtD//YKU0VttwVoSyG47/Pe7ix8jv8BKs7gHbIAwA",
+].map(data => Buffer.from(data, "base64"));
+
+type MockImage = typeof imageTable.$inferInsert & { imageData: Buffer };
 const mockImages: MockImage[] = [
   {
     id: "image-1",
     eventId: "birthday",
-    imageData: "",
+    isApproved: null,
+    imageData: mockImageData[0]!,
+  },
+  {
+    id: "image-2",
+    eventId: "birthday",
+    isApproved: true,
+    imageData: mockImageData[1]!,
+  },
+  {
+    id: "image-3",
+    eventId: "wedding",
+    isApproved: true,
+    imageData: mockImageData[2]!,
+  },
+  {
+    id: "image-4",
+    eventId: "wedding",
+    isApproved: false,
+    imageData: mockImageData[3]!,
   },
 ];
 
@@ -59,6 +85,8 @@ afterEach(async () => {
 
 describe("ImageService", () => {
   it("Temp", async () => {
-    expect(await imageService.getImages("birthday").getOrThrow()).toHaveLength(1);
+    expect(await imageService.getImages("birthday").getOrThrow()).toHaveLength(2);
+    await imageService.uploadImage("birthday", mockImageData[3]!).getOrThrow();
+    expect(await imageService.getImages("birthday").getOrThrow()).toHaveLength(3);
   });
 });
