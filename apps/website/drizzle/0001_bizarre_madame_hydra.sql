abdb91012fcfd1319c1802f4900149d08bffb026
Igor Iwanicki <igor.iwanicki@protonmail.com>
2026-02-23 15:09:57 +0100

    regenerate db schema

--- apps/website/drizzle/0001_bizarre_madame_hydra.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `images` (
-	`id` text PRIMARY KEY NOT NULL,
-	`eventId` text NOT NULL,
-	`createdAt` integer NOT NULL,
-	`updatedAt` integer NOT NULL,
-	FOREIGN KEY (`eventId`) REFERENCES `events`(`id`) ON UPDATE no action ON DELETE no action
-);
