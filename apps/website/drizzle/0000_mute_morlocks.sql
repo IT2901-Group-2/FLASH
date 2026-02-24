dd989d9d63d40966a198b99c8c517b03133ab27e
Igor Iwanicki <igor.iwanicki@protonmail.com>
2026-02-24 14:13:23 +0100

    Feat: Image upload endpoints (#106)
    * initial setup
    
    * cleanup
    
    * fix upload error
    
    * imageService comments
    
    * resize uploaded images
    
    * get images with available sizes
    
    * Revert "get images with available sizes"
    
    This reverts commit 734df171a010ce863ac88e1eb4bc5edb8f516f2f.
    
    * Revert "resize uploaded images"
    
    This reverts commit c85a091f8b6d55332c8072b2a97155c9b2780256.
    
    * code cleanup
    
    * regenerate db schema
    
    * delete image
    
    * update image
    
    * regenerate db schema
    
    * updateImage comment
    
    * imageService initial test setup
    
    * imageService test add mock data
    
    * filter images by approval status
    
    * imageService getImages tests
    
    * imageService downloadImage tests
    
    * imageService uploadImage tests
    
    * imageService updateImage tests
    
    * imageService deleteImage tests
    
    * clean up approval status filter

--- apps/website/drizzle/0000_mute_morlocks.sql
+++ apps/website/drizzle/0000_mute_morlocks.sql
@@ -9,9 +9,17 @@ CREATE TABLE `events` (
 	`moderatorCode` text NOT NULL,
 	`createdAt` integer NOT NULL,
 	`updatedAt` integer NOT NULL,
-	`isArchived` integer DEFAULT false,
+	`isArchived` integer DEFAULT false NOT NULL,
 	CONSTRAINT "dateConstraint" CHECK("events"."startDate" <= "events"."endDate")
 );
 --> statement-breakpoint
 CREATE UNIQUE INDEX `events_guestCode_unique` ON `events` (`guestCode`);--> statement-breakpoint
-CREATE UNIQUE INDEX `events_moderatorCode_unique` ON `events` (`moderatorCode`);
+CREATE UNIQUE INDEX `events_moderatorCode_unique` ON `events` (`moderatorCode`);--> statement-breakpoint
+CREATE TABLE `images` (
+	`id` text PRIMARY KEY NOT NULL,
+	`eventId` text NOT NULL,
+	`isApproved` integer,
+	`createdAt` integer NOT NULL,
+	`updatedAt` integer NOT NULL,
+	FOREIGN KEY (`eventId`) REFERENCES `events`(`id`) ON UPDATE no action ON DELETE no action
+);
---
+++ apps/website/src/services/imageService.ts
@@ -0,0 +1,185 @@
+import { FileStorage } from "file-storage";
+import { DatabaseService, dbService } from "./databaseService";
+import { AsyncResult, Result } from "typescript-result";
+import { GetImages, Image, imageTable, UpdateImage } from "@/db";
+import sharp, { Sharp, SharpInput } from "sharp";
+import ShortUniqueId from "short-unique-id";
+import { getFirstRow } from "@/lib/utils/sql";
+import { and, eq, inArray, isNull } from "drizzle-orm";
+import { storage } from "@/config";
+
+const uid = new ShortUniqueId();
+
+export class ImageService {
+  private readonly dbService: DatabaseService;
+  private readonly storage: FileStorage;
+
+  constructor(dbService: DatabaseService, storage: FileStorage) {
+    this.dbService = dbService;
+    this.storage = storage;
+  }
+
+  static readonly MAX_IMAGE_SIZE = 4 * 1024 * 1024;
+
+  /**
+   * Validates the image metadata using `sharp`.
+   * Checks that `sharp` is able to open the image file and that the size of the image does not exceed `ImageService.MAX_IMAGE_SIZE`.
+   *
+   * @param sharpImage `sharp` image to validate
+   * @returns A result with the provided `sharp` image for chaining or an error if the image is invalid.
+   */
+  private validateImage(sharpImage: Sharp): AsyncResult<Sharp, Error> {
+    return Result.fromAsyncCatching(sharpImage.clone().metadata()).map(meta => {
+      if (meta.size === undefined) {
+        return Result.error(new Error("Unable to determine image size"));
+      }
+
+      if (meta.size > ImageService.MAX_IMAGE_SIZE) {
+        return Result.error(new Error("Image exceeded the max image size"));
+      }
+
+      return Result.ok(sharpImage);
+    });
+  }
+
+  /**
+   * Returns a list of all images associated with the specified event.
+   *
+   * @param eventId The id of the event.
+   * @returns A result containing a list of `Image` objects or an error.
+   */
+  getImages(
+    eventId: string,
+    { id, approval }: GetImages = {}
+  ): AsyncResult<Image[], Error> {
+    return Result.try(() =>
+      this.dbService.db
+        .select()
+        .from(imageTable)
+        .where(
+          and(
+            eq(imageTable.eventId, eventId),
+            id !== undefined ? inArray(imageTable.id, id) : undefined,
+            approval !== undefined && approval !== "pending"
+              ? eq(imageTable.isApproved, approval === "approved")
+              : undefined,
+            approval === "pending" ? isNull(imageTable.isApproved) : undefined
+          )
+        )
+    );
+  }
+
+  /**
+   * Downloads the image with the specified id.
+   * Will fail if the image does not exist or does not belong to the specified event.
+   *
+   * @param eventId The id of the event the image belongs to.
+   * @param imageId The id of the image to download.
+   * @returns A result containing the downloaded image as a `Buffer` or an error
+   */
+  downloadImage(
+    eventId: string,
+    imageId: string
+  ): AsyncResult<Buffer<ArrayBufferLike>, Error> {
+    return Result.try(() =>
+      this.dbService.db
+        .select()
+        .from(imageTable)
+        .where(and(eq(imageTable.eventId, eventId), eq(imageTable.id, imageId)))
+        .limit(1)
+    )
+      .map(rows =>
+        getFirstRow(
+          rows,
+          `Image with id ${imageId} does not exist on event with id ${eventId}`
+        )
+      )
+      .map(() => this.storage.read(`${imageId}.webp`));
+  }
+
+  /**
+   * Uploads the given image blob to the specified event.
+   *
+   * @param eventId The id of the event to upload the image into.
+   * @param image The image to upload.
+   * @returns A result containing the newly inserted `Image` object or an error.
+   */
+  uploadImage(eventId: string, image: SharpInput): AsyncResult<Image, Error> {
+    const imageId = uid.rnd();
+
+    return Result.try(() => sharp(image))
+      .map(sharpImage => this.validateImage(sharpImage))
+      .mapCatching(sharpImage => sharpImage.clone().webp().toBuffer())
+      .map(buff => this.storage.write(`${imageId}.webp`, buff))
+      .map(() =>
+        Result.try(() =>
+          this.dbService.db
+            .insert(imageTable)
+            .values({ id: imageId, eventId })
+            .returning()
+        )
+          .map(rows => getFirstRow(rows, "Failed to upload image"))
+          .onSuccess(() => this.dbService.flush())
+          .onFailure(async () => {
+            await this.storage.rm(`${imageId}.webp`).getOrNull();
+          })
+      );
+  }
+
+  /**
+   * Updates the image with the specified id.
+   * Will fail if the image does not exist or does not belong to the specified event.
+   *
+   * @param eventId The id of the event the image belongs to.
+   * @param imageId The id of the image to update.
+   * @param data The data to update the image with.
+   * @returns A result containing the updated `Ìmage` object or an error
+   */
+  updateImage(
+    eventId: string,
+    imageId: string,
+    data: UpdateImage
+  ): AsyncResult<Image, Error> {
+    return Result.try(() =>
+      this.dbService.db
+        .update(imageTable)
+        .set(data)
+        .where(and(eq(imageTable.eventId, eventId), eq(imageTable.id, imageId)))
+        .returning()
+    )
+      .map(rows =>
+        getFirstRow(
+          rows,
+          `Image with id ${imageId} does not exist on event with id ${eventId}`
+        )
+      )
+      .onSuccess(() => this.dbService.flush());
+  }
+
+  /**
+   * Deletes the image with the specified id.
+   * Will fail if the image does not exist or does not belong to the specified event.
+   *
+   * @param eventId The id of the event the image belongs to.
+   * @param imageId The id of the image to delete.
+   * @returns A result containing the deleted `Ìmage` object or an error
+   */
+  deleteImage(eventId: string, imageId: string): AsyncResult<Image, Error> {
+    return Result.try(() =>
+      this.dbService.db
+        .delete(imageTable)
+        .where(and(eq(imageTable.eventId, eventId), eq(imageTable.id, imageId)))
+        .returning()
+    )
+      .map(rows =>
+        getFirstRow(
+          rows,
+          `Image with id ${imageId} does not exist on event with id ${eventId}`
+        )
+      )
+      .onSuccess(() => this.dbService.flush())
+      .map(row => this.storage.rm(`${row.id}.webp`).map(() => row));
+  }
+}
+
+export const imageService = new ImageService(dbService, storage);
