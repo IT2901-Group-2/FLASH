2e2f32f9fc13f0897d5f70b11e5764f3cf2f8a43
Gnister <sscharer@online.no>
2026-03-02 15:46:29 +0100

    fetch all joined events

--- apps/website/src/components/RememberedEvents/RememberedEvents.tsx
+++ apps/website/src/components/RememberedEvents/RememberedEvents.tsx
@@ -1,9 +1,12 @@
+"use client";
+
 import { useEventsQuery } from "@/hooks/useEvents";
 import { EventDTO } from "@/types/eventTypes";
 import { Calendar, ChevronRight } from "lucide-react";
 import { Card, Title } from "ui";
 import styles from "./RememberedEvents.module.css";
 import { useRouter } from "next/navigation";
+import { getAllJoinedEvents } from "@/hooks/useRememberEvents";
 
 const RememberedEvent = ({ name, uploadLimit, id }: EventDTO) => {
   const navigation = useRouter();
@@ -19,8 +22,15 @@ const RememberedEvent = ({ name, uploadLimit, id }: EventDTO) => {
 };
 
 const RememberedEvents = () => {
-  const events = useEventsQuery().data;
+  console.log(typeof window);
+  const allEventIDs = getAllJoinedEvents();
+
+  const events = useEventsQuery({
+    id: allEventIDs.length !== 0 ? allEventIDs : ["A"],
+  }).data;
   if (!events) return;
+
+  if (events.length === 0) return;
 
   return (
     <Card className={styles.card}>
