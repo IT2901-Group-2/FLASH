5775c0efef22229daa3cda14aca65a0d957ff4c6
karlfrostrud <frostrud6@gmail.com>
2026-03-02 16:35:37 +0100

    Add download qr code functionailty to review step component (#151)
    * Add download qr code functionailty to review step component
    
    * Added dialog component
    
    * Added dialog to show QRcode
    
    * Fix  ReviewStep on mobile
    
    ---------
    
    Co-authored-by: Karl Frostrud <karlfrostrud@dhcp-10-220-231-38.wlan.ntnu.no>
    Co-authored-by: Gnister <sscharer@online.no>

--- packages/ui/src/components/index.ts
+++ packages/ui/src/components/index.ts
@@ -1,6 +1,7 @@
 export * from "./Button";
 export * from "./Loader";
 export * from "./Card";
+export * from "./Dialog";
 export * from "./ProgressBar";
 export * from "./ProgressDots";
 export * from "./Input";
