9764eae62fc74e13c35cf9ce52239ee35f824594
Gnister <sscharer@online.no>
2026-03-03 14:18:36 +0100

    Fix namespace

--- packages/ui/src/components/types/theme.ts
+++ packages/ui/src/components/types/theme.ts
@@ -1,2 +1,12 @@
 import type { ColorRole } from "@flash/tokens/types";
 export type ColorName = ColorRole;
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace React {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    interface HTMLAttributes<T> {
+      "data-color"?: ColorName;
+    }
+  }
+}
