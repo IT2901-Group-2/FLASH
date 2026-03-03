522302b0956ae1b0403cac897aae69d61e276158
Gnister <sscharer@online.no>
2026-03-03 21:40:55 +0100

    Done

--- apps/website/src/components/CreateEventCard/Steps/ReviewStep.tsx
+++ apps/website/src/components/CreateEventCard/Steps/ReviewStep.tsx
@@ -1,13 +1,22 @@
 import { useRef, useState } from "react";
-import { StepProps } from "../CreateEventCard";
-import { Title, SegmentedControl, QRDisplay, Button, Input, Loader } from "ui";
+import {
+  Title,
+  SegmentedControl,
+  QRDisplay,
+  Button,
+  Input,
+  Loader,
+  DropdownControl,
+} from "ui";
 import { Copy, Download } from "lucide-react";
 import styles from "./Steps.module.css";
 import { useTranslations } from "next-intl";
 import { downloadQrSvg } from "@/utils/downloadqrcode";
 import { ReviewStepProps } from "./types";
+import { useRouter } from "next/navigation";
 
 const ReviewStep = ({ status, result }: ReviewStepProps) => {
+  const navigation = useRouter();
   const t = useTranslations("admin.dashboard.event.create.review");
   const [shareRole, setShareRole] = useState<string>("guest");
   const qrContainerRef = useRef<HTMLDivElement | null>(null);
@@ -34,30 +43,70 @@ const ReviewStep = ({ status, result }: ReviewStepProps) => {
       <Title size="medium" description={t("description")}>
         {t("title")}
       </Title>
-      <SegmentedControl onChange={setShareRole} value={shareRole} fill>
-        <SegmentedControl.Item label={t("guest.name")} value="guest" />
-        <SegmentedControl.Item label={t("moderator.name")} value="moderator" />
-      </SegmentedControl>
-      <div className={styles.infoContainer}>
-        <div className={styles.QRCodeContainer} ref={qrContainerRef}>
-          <QRDisplay value={displayLink} code={displayCode} />
-          <Button variant="secondary" icon={<Download />} onClick={handleDownloadQR}>
-            {t("download")}
-          </Button>
-        </div>
-        <div className={styles.linkContainer}>
-          <Title size="medium" description={t("guest.linkDescription")}>
-            {t("guest.linkTitle")}
-          </Title>
-          <Input
-            aria-label="link"
-            readOnly
-            value={displayLink}
-            icon={<Copy />}
-            iconPosition="right"
-          />
-        </div>
-      </div>
+      <DropdownControl onChange={setShareRole} value={shareRole} fill>
+        <DropdownControl.Item
+          label={t("guest.name")}
+          value="guest"
+          content={
+            <div className={styles.infoContainer}>
+              <div className={styles.QRCodeContainer} ref={qrContainerRef}>
+                <QRDisplay value={displayLink} code={displayCode} />
+                <Button
+                  variant="secondary"
+                  icon={<Download />}
+                  onClick={handleDownloadQR}
+                >
+                  {t("download")}
+                </Button>
+              </div>
+              <div className={styles.linkContainer} data-color="neutral">
+                <Title size="medium" description={t("guest.linkDescription")}>
+                  {t("guest.linkTitle")}
+                </Title>
+                <Input
+                  aria-label="link"
+                  readOnly
+                  value={displayLink}
+                  icon={<Copy />}
+                  iconPosition="right"
+                  fill
+                />
+              </div>
+            </div>
+          }
+        />
+        <DropdownControl.Item
+          label={t("moderator.name")}
+          value="moderator"
+          content={
+            <div className={styles.infoContainer}>
+              <div className={styles.QRCodeContainer} ref={qrContainerRef}>
+                <QRDisplay value={displayLink} code={displayCode} />
+                <Button
+                  variant="secondary"
+                  icon={<Download />}
+                  onClick={handleDownloadQR}
+                >
+                  {t("download")}
+                </Button>
+              </div>
+              <div className={styles.linkContainer} data-color="neutral">
+                <Title size="medium" description={t("moderator.linkDescription")}>
+                  {t("moderator.linkTitle")}
+                </Title>
+                <Input
+                  aria-label="link"
+                  readOnly
+                  value={displayLink}
+                  icon={<Copy />}
+                  iconPosition="right"
+                  fill
+                />
+              </div>
+            </div>
+          }
+        />
+      </DropdownControl>
     </>
   );
 };
