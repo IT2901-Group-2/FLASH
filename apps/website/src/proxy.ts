e88f50630956c31c792b46944ccf152bdac0d6c5
Igor Iwanicki <igor.iwanicki@protonmail.com>
2026-03-10 17:30:42 +0100

    proxy cleanup

--- apps/website/src/proxy.ts
+++ apps/website/src/proxy.ts
@@ -8,7 +8,33 @@ import { cookies } from "next/headers";
 const handleI18nRouting = createMiddleware(routing);
 
 /**
+ * Checks whether the request was made to a route that requires event session authentication.
+ *
+ * @param request The request to check.
+ * @param A boolean indicating whether the request is sent to an event route or not.
+ */
+function isEventRoute(request: NextRequest): boolean {
+  return /^\/.*\/events\/.+/.test(request.nextUrl.pathname);
+}
+
+/**
+ * Retrieves the eventId from the route path.
+ * Only usable within event routes, check with `isEventRoute` before invoking.
+ *
+ * @param request The request to retrieve the eventId from.
+ * @returns The eventId of the event the request is accessing.
+ */
+function getEventId(request: NextRequest): string {
+  const eventId = /^\/.*\/events\/([^\/]*)/.exec(request.nextUrl.pathname)?.[1];
+  if (eventId === undefined) {
+    throw new Error("getEventId must only be invoked within an event route");
+  }
+  return eventId;
+}
+
+/**
  * Checks that the request carries a valid EventCookie.
+ * If the checked cookie is malformed it will be cleared.
  *
  * @param eventId The event id to check the EventCookie for.
  * @returns A promise that resolves to true if the cookie is valid, false otherwise.
@@ -26,11 +52,9 @@ async function checkEventCookie(eventId: string): Promise<boolean> {
 }
 
 export default async function proxy(request: NextRequest): Promise<NextResponse> {
-  const eventId = /\/events\/(.*)$/g.exec(request.nextUrl.pathname)?.[1];
-
-  if (eventId !== undefined) {
-    const validCookie = await checkEventCookie(eventId);
-    if (!validCookie) {
+  if (isEventRoute(request)) {
+    const isAuthenticated = await checkEventCookie(getEventId(request));
+    if (!isAuthenticated) {
       return NextResponse.redirect(new URL("/", request.url));
     }
   }
