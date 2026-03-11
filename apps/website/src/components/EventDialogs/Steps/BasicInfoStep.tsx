bb361edeee1f16d63193b8e018404944f140b5a8
Gnister <sscharer@online.no>
2026-03-11 14:58:35 +0100

    Update some css

--- apps/website/src/components/EventDialogs/Steps/BasicInfoStep.tsx
+++ apps/website/src/components/EventDialogs/Steps/BasicInfoStep.tsx
@@ -1,5 +1,5 @@
 import { Calendar } from "lucide-react";
-import { Title, TextField } from "ui";
+import { Title, TextField, Textarea } from "ui";
 import { useTranslations } from "next-intl";
 import { StepProps } from "./types";
 import styles from "./Steps.module.css";
@@ -8,38 +8,47 @@ import {
   formatTimeForInput,
   makeDateTimeHandler,
 } from "@/utils/date-utils";
+import { useForm } from "react-hook-form";
 
 export const BasicInfoStep = ({ formData, updateFormData }: StepProps) => {
-  const tStep = useTranslations("admin.dashboard.event.create.basics");
-  const tFields = useTranslations("common.fields");
+  const t = useTranslations();
 
   const startDateValue = formData.startDate.toISOString().split("T")[0];
 
+  const {
+    register,
+    formState: { errors },
+  } = useForm();
+
   return (
     <>
-      <Title description={tStep("description")}>{tStep("title")}</Title>
+      <Title description={t("admin.dashboard.event.create.basics.description")}>
+        {t("admin.dashboard.event.create.basics.title")}
+      </Title>
       <TextField
-        value={formData.name}
-        onChange={e => updateFormData("name", e.target.value)}
-        label={tFields("eventName")}
-        aria-label={tFields("eventName")}
-        minLength={3}
+        label={t("common.fields.eventName")}
+        aria-label={t("common.fields.eventName")}
+        {...register("name", {
+          maxLength: { value: 255, message: "" }, //TODO
+          required: "", //TODO
+        })}
         required
         data-testid="name"
       />
-      <TextField
+      <Textarea
         value={formData.description}
-        onChange={e => updateFormData("description", e.target.value)}
-        label={tFields("eventDescription")}
-        aria-label={tFields("eventDescription")}
+        {...register("descriprion")}
+        label={t("common.fields.eventDescription")}
+        aria-label={t("common.fields.eventDescription")}
         data-testid="description"
+        resize="vertical"
       />
       <div className={styles.timeContainer}>
         <TextField
           value={formatDateForInput(formData.startDate)}
           onChange={makeDateTimeHandler("startDate", "date", formData, updateFormData)}
-          label={tFields("startDate")}
-          aria-label={tFields("startDate")}
+          label={t("common.fields.startDate")}
+          aria-label={t("common.fields.startDate")}
           icon={<Calendar />}
           required
           data-testid="startDate"
@@ -48,32 +57,20 @@ export const BasicInfoStep = ({ formData, updateFormData }: StepProps) => {
         <TextField
           value={formatTimeForInput(formData.startDate)}
           onChange={makeDateTimeHandler("startDate", "time", formData, updateFormData)}
-          label={tFields("startTime")}
-          aria-label={tFields("startTime")}
+          label={t("common.fields.startTime")}
+          aria-label={t("common.fields.startTime")}
           type="time"
           icon={<Calendar />}
           required
         />
         <TextField
-          value={formatDateForInput(formData.endDate)}
-          onChange={makeDateTimeHandler("endDate", "date", formData, updateFormData)}
-          label={tFields("endDate")}
-          aria-label={tFields("endDate")}
+          label={"Duration"}
+          aria-label={t("common.fields.endDate")}
           min={startDateValue}
-          // type="date"
           icon={<Calendar />}
           required
           data-testid="endDate"
           // fill
-        />
-        <TextField
-          value={formatTimeForInput(formData.endDate)}
-          onChange={makeDateTimeHandler("endDate", "time", formData, updateFormData)}
-          label={tFields("endTime")}
-          aria-label={tFields("endTime")}
-          type="time"
-          icon={<Calendar />}
-          required
         />
       </div>
     </>
