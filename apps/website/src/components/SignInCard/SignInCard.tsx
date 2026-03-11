8b99a95725563b5613056ad08e1ed867ad2083ec
Philip Torvund Bennett <philitb@stud.ntnu.no>
2026-03-11 14:34:40 +0100

    Connected login to signInCard

--- apps/website/src/components/SignInCard/SignInCard.tsx
+++ apps/website/src/components/SignInCard/SignInCard.tsx
@@ -3,10 +3,20 @@ import styles from "./SignInCard.module.css";
 import { useTranslations } from "next-intl";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
+import { useLoginMutation } from "@/hooks/useAuth";
+import { useState } from "react";
 
 export default function SignInCard() {
   const navigate = useRouter();
   const t = useTranslations("admin.login.signIn");
+  const [password, setPassword] = useState("");
+  const { mutate: login, isPending, isError } = useLoginMutation();
+
+  const handleSubmit = (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    login({ password }, { onSuccess: () => navigate.push("/admin/dashboard") });
+  };
+
   return (
     <Card className={styles.card}>
       <Title
@@ -18,24 +28,27 @@ export default function SignInCard() {
       >
         {t("title")}
       </Title>
-      <Input
-        className={styles.inputComponent}
-        aria-label="password"
-        label={t("inputLabel")}
-        required
-        type="password"
-        id="passwordField"
-        visualSize="medium"
-        placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;"
-      />
-      <Button
-        className={styles.buttonComponent}
-        data-color="brand-purple"
-        fill
-        onClick={() => navigate.push("/admin/dashboard")}
-      >
-        {t("buttonTitle")}
-      </Button>
+      <form onSubmit={handleSubmit}>
+        <Input
+          className={styles.inputComponent}
+          aria-label="password"
+          label={t("inputLabel")}
+          required
+          type="password"
+          id="passwordField"
+          visualSize="medium"
+          placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;"
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+        />
+        <Button
+          className={styles.buttonComponent}
+          data-color="brand-purple"
+          fill
+          disabled={isPending}
+          type="submit"
+        ></Button>
+      </form>
       <span>
         {t("linkToGuest")}{" "}
         <Link role="link" href={"/"}>
