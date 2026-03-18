472adce8507668c2a20f062ba48e61f1e07c8be0
Gnister <sscharer@online.no>
2026-03-18 14:40:53 +0100

    Removed comments

--- apps/website/__mocks__/index.ts
+++ apps/website/__mocks__/index.ts
@@ -1,17 +1,6 @@
-// --- Wrappers / render helpers ---
 export * from "./wrappers.mock";
-
-// --- Shared router mock ---
 export * from "./router.mock";
-
-// --- Fetch helpers ---
 export * from "./fetch.mock";
-
-// --- Object factories ---
 export * from "./factories";
-
-// --- Hook return-value helpers ---
 export * from "./hooks";
-
-// --- vi.mock() factory functions ---
 export * from "./modules";
