227c961ee50f494a565b8145ce271aa436395227
Gnister <sscharer@online.no>
2026-03-24 10:33:35 +0100

    fix card stories

--- packages/ui/src/components/Card/Card.stories.tsx
+++ packages/ui/src/components/Card/Card.stories.tsx
@@ -61,8 +61,8 @@ export const WithContent: Story = {
 export const Colors: Story = {
   render: () => (
     <>
-      <Card data-color="primary">Neutral</Card>
-      <Card data-color="secondary">Neutral</Card>
+      <Card data-color="primary">Primary</Card>
+      <Card data-color="secondary">Secondary</Card>
       <Card data-color="brand-purple">Brand Purple</Card>
       <Card data-color="accent">Accent</Card>
       <Card data-color="success">Success</Card>
