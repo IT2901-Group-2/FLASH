a4d16906a9903de69ba69ecefc32ae6b6275ebcd
Gnister <sscharer@online.no>
2026-04-06 13:02:24 +0200

    removed unused import

--- apps/website/src/components/EventDialogs/EditEventDialog.test.tsx
+++ apps/website/src/components/EventDialogs/EditEventDialog.test.tsx
@@ -1,5 +1,5 @@
 import { eventHooksMock, makeEvent } from "@test-config";
-import { render, screen, waitFor } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { describe, it, expect, vi, beforeEach } from "vitest";
 import EditEventDialog from "./EditEventDialog";
