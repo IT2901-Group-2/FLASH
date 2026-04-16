6c51b12b29a153af9f1232fab5ea389e6fe02f0d
Gnister <sscharer@online.no>
2026-04-16 17:35:46 +0200

    `debaounce` JSDoc

--- packages/ui/src/util/helpers/debounce.ts
+++ packages/ui/src/util/helpers/debounce.ts
@@ -2,6 +2,17 @@
 
 type Debounced<T extends unknown[]> = ((...args: T) => void) & { clear: () => void };
 
+/**
+ * Creates a debounced function that delays invoking the provided function
+ * until after a specified wait time has elapsed since the last time the
+ * debounced function was invoked. Optionally, the function can be invoked
+ * immediately on the leading edge of the timeout instead of the trailing edge.
+ *
+ * @param func The function to debounce.
+ * @param wait The number of milliseconds to delay before invoking the function after the last call.
+ * @param leading If true, the function will be invoked on the leading edge of the timeout instead of the trailing edge.
+ * @returns A debounced version of the provided function.
+ */
 function debounce<T extends unknown[]>(
   func: (...args: T) => void,
   wait: number,
