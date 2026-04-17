50f663b4bf02bf63e162a413792023178feeeaf1
Gnister <sscharer@online.no>
2026-04-17 11:46:10 +0200

    Revert "`useIsMouned` docs"
    This reverts commit d04d8e2c6ce058f9625967b6a5c240eec8bd208c.

--- apps/website/src/hooks/useIsMounted.ts
+++ apps/website/src/hooks/useIsMounted.ts
@@ -1,20 +1,5 @@
 import { useSyncExternalStore } from "react";
 
-/**
- * A hook that returns `true` if the component is currently mounted, and `false` otherwise.
- * This can be used to avoid setting state on an unmounted component.
- *
- * @returns `true` if the component is mounted, `false` otherwise
- *
- * @example
- * const MyComponent = () => {
- *   const isMounted = useIsMounted();
- *   useEffect(() => {
- *     fetchData().then(data => {
- *       if (isMounted) setData(data);
- *    });
- *  });
- */
 export function useIsMounted() {
   return useSyncExternalStore(
     () => () => {},
