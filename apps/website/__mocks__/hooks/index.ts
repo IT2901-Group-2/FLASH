e3a659d0381d441c6e459d432bf3f4d5bbdb8bd8
Gnister <sscharer@online.no>
2026-04-23 09:50:05 +0200

    Added more mocks

--- apps/website/__mocks__/hooks/index.ts
+++ apps/website/__mocks__/hooks/index.ts
@@ -4,3 +4,6 @@ export * from "./useImages.mock";
 export * from "./useQuery.mock";
 export * from "./useLanguage.mock";
 export * from "./useFileUpload.mock";
+export * from "./useTheme.mock";
+export * from "./useIsMounted.mock";
+export * from "./useAuth.mock";
--- apps/website/__mocks__/modules/index.ts
+++ apps/website/__mocks__/modules/index.ts
@@ -2,3 +2,4 @@ export * from "./contexts.mock";
 export * from "./flash-ui.mock";
 export * from "./useEvents.mock";
 export * from "./useImages.mock";
+export * from "./useAuth.mock";
