0195caad729a2ee99e1ea2eee847295ed305b408
Sindre Augustin Schärer <82974914+GnisterDev@users.noreply.github.com>
2026-04-23 13:16:44 +0200

    More mocks (#413)
    * Added more mocks
    
    * QrScanner mock

--- apps/website/__mocks__/modules/index.ts
+++ apps/website/__mocks__/modules/index.ts
@@ -2,3 +2,5 @@ export * from "./contexts.mock";
 export * from "./flash-ui.mock";
 export * from "./useEvents.mock";
 export * from "./useImages.mock";
+export * from "./useAuth.mock";
+export * from "./QrScanner.mock";
