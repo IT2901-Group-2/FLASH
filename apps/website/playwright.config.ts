3db295eb1f7f43f3c08c1f094d374ecadfa6b101
Gnister <sscharer@online.no>
2026-04-27 14:38:22 +0200

    Update playwright config

--- apps/website/playwright.config.ts
+++ apps/website/playwright.config.ts
@@ -31,7 +31,7 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
 
-    /* Screenshot last frame before test fails */
+    headless: true,
     screenshot: "only-on-failure",
   },
 
