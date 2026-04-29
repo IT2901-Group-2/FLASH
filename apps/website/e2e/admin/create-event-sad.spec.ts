b890e126cec263b90728fd7563dc824e2133c1f1
Gnister <sscharer@online.no>
2026-04-29 16:30:15 +0200

    Update create-event-sad

--- apps/website/e2e/admin/create-event-sad.spec.ts
+++ apps/website/e2e/admin/create-event-sad.spec.ts
@@ -5,7 +5,7 @@ test.use({
 });
 
 test("test", async ({ page }) => {
-  await page.routeFromHAR("e2e/hars/create-event-sad.har", {
+  await page.routeFromHAR("e2e/hars/create-event-sad.har.zip", {
     url: "**/api/**",
   });
   await page.goto("http://localhost:3000/no");
@@ -13,37 +13,35 @@ test("test", async ({ page }) => {
   await page.getByRole("textbox", { name: "Passord" }).click();
   await page.getByRole("textbox", { name: "Passord" }).fill("Default");
   await page.getByRole("button", { name: "Logg inn" }).click();
-
   await page.getByRole("button", { name: "Lag nytt event" }).click();
-  await page.getByRole("button", { name: "Neste" }).click();
+  await page.getByRole("textbox", { name: "Eventnavn" }).fill("Playwright Sad Event");
+  await page.getByRole("textbox", { name: "Eventnavn" }).press("ControlOrMeta+a");
+  await page.getByRole("textbox", { name: "Eventnavn" }).fill("");
   await expect(page.getByText("Dette feltet er påkrevd")).toBeVisible();
   await page.getByRole("radio", { name: "Spesifikke tider" }).click();
-
   await page.getByRole("textbox", { name: "Starttid" }).click();
   await page.getByRole("textbox", { name: "Starttid" }).press("Shift+Tab");
   await page.getByRole("textbox", { name: "Starttid" }).fill("18:00");
   await page.getByRole("button", { name: "Neste" }).click();
   await expect(page.getByText("Starttid må være før sluttid.")).toBeVisible();
-
+  await page.getByRole("textbox", { name: "Eventnavn" }).click();
+  await page.getByRole("textbox", { name: "Eventnavn" }).fill("Sad Event");
   await page.getByRole("textbox", { name: "Sluttid" }).click();
   await page.getByRole("textbox", { name: "Sluttid" }).press("Shift+Tab");
   await page.getByRole("textbox", { name: "Sluttid" }).fill("21:00");
   await page.getByRole("button", { name: "Neste" }).click();
-  await page
-    .getByText(
-      "Eventnavn*Navnet på eventet ditt. Lag et unikt et.Dette feltet er påkrevd"
-    )
-    .click();
-  await page.getByRole("textbox", { name: "Eventnavn" }).fill("Hello World");
-  await page.getByRole("button", { name: "Neste" }).click();
-  await page.getByRole("textbox", { name: "*" }).click();
-  await page.getByRole("textbox", { name: "*" }).fill("");
-
+  await page.getByRole("radio", { name: "Uendelig" }).click();
+  await page.getByRole("radio", { name: "Begrenset" }).click();
+  await page.getByRole("button", { name: "Opprett" }).click();
   await expect(
     page.getByText("Velg en opplastningsgrense", { exact: true })
   ).toBeVisible();
-  await page.getByRole("textbox", { name: "*" }).click();
-  await page.getByRole("textbox", { name: "*" }).fill("10");
+  await page.getByRole("radio", { name: "Uendelig" }).click();
+  await page.getByRole("switch", { name: "Godkjenn bilder automatisk" }).check();
+  await page.getByRole("switch", { name: "La gjester se alle bilder" }).uncheck();
   await page.getByRole("button", { name: "Opprett" }).click();
+  await page.getByTestId("copy-button").click();
+  await page.getByRole("radio", { name: "Moderator" }).click();
+  await page.getByTestId("copy-button").click();
   await page.getByRole("button", { name: "Fullfør" }).click();
 });
