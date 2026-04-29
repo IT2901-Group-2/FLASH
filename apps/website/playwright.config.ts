ebe709f5eeacebfb3755bb3db54ff12fd6973f71
Gnister <sscharer@online.no>
2026-04-29 19:31:30 +0200

    Set hard to 60s

--- apps/website/playwright.config.ts
+++ apps/website/playwright.config.ts
@@ -44,7 +44,7 @@ export default defineConfig({
     // ],
   ],
 
-  timeout: process.env.CI ? 60 * 1000 : 30 * 1000,
+  timeout: 60 * 1000,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
