421f8a6f520f34b22631ae669d119f41e279ef8d
Gnister <sscharer@online.no>
2026-04-29 17:11:45 +0200

    Fixes

--- apps/website/e2e/admin/admin-upload.spec.ts
+++ apps/website/e2e/admin/admin-upload.spec.ts
@@ -5,22 +5,22 @@ test.use({
 });
 
 test("test", async ({ page }) => {
-  await page.routeFromHAR("e2e/hars/admin-upload.har.zip", { url: "**/api/**" });
+  await page.routeFromHAR("e2e/hars/admin-upload.har.zip", {
+    url: "**/api/**",
+  });
   await page.goto("http://localhost:3000/no");
   await page.getByRole("link", { name: "Administrator" }).click();
   await page.getByRole("textbox", { name: "Passord" }).click();
   await page.getByRole("textbox", { name: "Passord" }).fill("Default");
   await page.getByRole("button", { name: "Logg inn" }).click();
-  await page
-    .getByText(
-      "Playwright Test29. apr. 2026, 00:00Bilder totalt 0Godkjent 0Venter 0Ingen"
-    )
-    .click();
+  await page.getByText("Playwright Test").click();
   await page.getByRole("button", { name: "Bli med" }).click();
   await page.getByRole("button", { name: "Last opp bilde" }).click();
-  await page.getByRole("button", { name: "Last opp bilde" }).setInputFiles("planets.jpg");
+  await page
+    .getByRole("button", { name: "Last opp bilde" })
+    .setInputFiles("pixel-night.png");
   await page.getByRole("radio", { name: "Dine bilder" }).click();
   await expect(
-    page.getByRole("button", { name: "Bilde 1 av 1 Ventende..." })
+    page.getByRole("button", { name: "Bilde 2 av 2 Ventende..." })
   ).toBeVisible();
 });
