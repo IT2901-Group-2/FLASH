a7e9bb2b23fcd880896c768f8eb6bff4a270414b
Gnister <sscharer@online.no>
2026-04-29 17:32:07 +0200

    admin moderate

---
+++ apps/website/e2e/admin/admin-moderate.spec.ts
@@ -0,0 +1,28 @@
+import { test } from "@playwright/test";
+
+test.use({
+  serviceWorkers: "block",
+});
+
+test("test", async ({ page }) => {
+  await page.routeFromHAR("e2e/hars/admin-moderate.har.zip", {
+    url: "**/api/**",
+  });
+
+  await page.goto("http://localhost:3000/no");
+  await page.getByRole("link", { name: "Administrator" }).click();
+  await page.getByRole("textbox", { name: "Passord" }).click();
+  await page.getByRole("textbox", { name: "Passord" }).fill("Default");
+  await page.getByRole("button", { name: "Logg inn" }).click();
+  await page.getByText("Playwright Test").click();
+  await page.getByRole("button", { name: "Bli med" }).click();
+  await page.getByRole("button", { name: "Moderer" }).click();
+  await page.getByRole("button", { name: "Velg" }).click();
+  await page.getByRole("button", { name: "Bilde 1 av" }).click();
+  await page.getByRole("button", { name: "Avvis valgte bilder" }).click();
+  await page.getByRole("radio", { name: "Avvist" }).click();
+  await page.getByRole("button", { name: "Velg" }).click();
+  await page.getByRole("button", { name: "Velg alle" }).click();
+  await page.getByRole("button", { name: "Godkjenn valgte bilder" }).click();
+  await page.getByRole("button", { name: "Gå tilbake" }).click();
+});
