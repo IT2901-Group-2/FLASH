23a4e44df9e25dab922549fd709c6cb94415eaeb
Gnister <sscharer@online.no>
2026-04-29 17:44:31 +0200

    fix errors

--- apps/website/e2e/admin/admin-moderate.spec.ts
+++ apps/website/e2e/admin/admin-moderate.spec.ts
@@ -16,11 +16,17 @@ test("test", async ({ page }) => {
   await page.getByRole("button", { name: "Logg inn" }).click();
   await page.getByText("Playwright Test").click();
   await page.getByRole("button", { name: "Bli med" }).click();
+
+  await new Promise(r => setTimeout(r, 1000));
+
   await page.getByRole("button", { name: "Moderer" }).click();
   await page.getByRole("button", { name: "Velg" }).click();
   await page.getByRole("button", { name: "Bilde 1 av" }).click();
   await page.getByRole("button", { name: "Avvis valgte bilder" }).click();
-  await page.getByRole("radio", { name: "Avvist" }).click();
+
+  await new Promise(r => setTimeout(r, 1000));
+
+  await page.getByText("Avvist").click();
   await page.getByRole("button", { name: "Velg" }).click();
   await page.getByRole("button", { name: "Velg alle" }).click();
   await page.getByRole("button", { name: "Godkjenn valgte bilder" }).click();
