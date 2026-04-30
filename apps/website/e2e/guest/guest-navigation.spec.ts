58ce625c5554db531f1647dbd28ad7774dcee962
Gnister <sscharer@online.no>
2026-04-30 09:27:17 +0200

    guest navigation

---
+++ apps/website/e2e/guest/guest-navigation.spec.ts
@@ -0,0 +1,18 @@
+import { test } from "@playwright/test";
+
+test.use({
+  serviceWorkers: "block",
+});
+
+test("test", async ({ page }) => {
+  await page.routeFromHAR("e2e/hars/guest-navigation.har.zip", { url: "**/api/**" });
+
+  await page.goto("http://localhost:3000/no");
+  await page.getByRole("textbox", { name: "Eventkode" }).click();
+  await page.getByRole("textbox", { name: "Eventkode" }).fill("TMN1N2");
+  await page.getByRole("button", { name: "Bli med" }).click();
+
+  await page.getByRole("textbox", { name: "Kallenavn" }).click();
+  await page.getByRole("textbox", { name: "Kallenavn" }).fill("Playwright Bot");
+  await page.getByRole("button", { name: "Bli med" }).click();
+});
