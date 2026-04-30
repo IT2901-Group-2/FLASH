78b4863400b4f659d1db3965b3695c0ddfbd61ac
Gnister <sscharer@online.no>
2026-04-30 08:55:44 +0200

    Update playwright config

--- apps/website/playwright.config.ts
+++ apps/website/playwright.config.ts
@@ -43,7 +43,7 @@ export default defineConfig({
     // ],
   ],
 
-  timeout: 60 * 1000,
+  timeout: 30 * 1000,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
