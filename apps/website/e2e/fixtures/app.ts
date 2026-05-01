9adca4d748508d00673143c8527beec765966af4
Igor Iwanicki <igor.iwanicki@protonmail.com>
2026-05-01 23:15:45 +0200

    zip db fixture

--- apps/website/e2e/fixtures/app.ts
+++ apps/website/e2e/fixtures/app.ts
@@ -3,6 +3,7 @@ import { spawn } from "child_process";
 import getPort from "get-port";
 import waitOn from "wait-on";
 import fs from "fs/promises";
+import AdmZip from "adm-zip";
 import path from "path";
 import os from "os";
 
@@ -11,7 +12,7 @@ const MOCK_DATE = "2026-05-01T10:00:00Z";
 export const test = base.extend<{ appUrl: string }>({
   appUrl: async ({ page }, use) => {
     const STORAGE_DIR = await fs.mkdtemp(path.join(os.tmpdir(), "flash-playwright-"));
-    await fs.cp(path.join(__dirname, "db"), STORAGE_DIR, { recursive: true });
+    new AdmZip(path.join(__dirname, "db.zip")).extractAllTo(STORAGE_DIR);
 
     await page.clock.install({ time: MOCK_DATE });
 
